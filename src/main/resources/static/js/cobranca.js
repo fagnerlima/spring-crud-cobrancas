b00aa134c3de45abe4d292087cd15dac648f21e1
Fagner Lima <contato@fagnerlima.pro.br>
2016-08-13 23:27:58 -0300

    Máscara de entrada de valores monetários (jQueryMaskMoney)

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,20 +1,28 @@
-// Exclusão de título
-$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
-	var button = $(event.relatedTarget);
-	var id = button.data('id');
-	var descricao = button.data('descricao');
-	
-	var modal = $(this);
-	var form = modal.find('form');
-	var baseurl = form.data('baseurl');
-	
-	if(!baseurl.endsWith('/'))
-		baseurl += '/';
-	
-	form.attr('action', baseurl + id);
-	modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <b>' + id + '</b> (<b>' + descricao + '</b>)?');
-});
-
 $(function() {
+	// Exclusão de título
+	$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
+		var button = $(event.relatedTarget);
+		var id = button.data('id');
+		var descricao = button.data('descricao');
+		
+		var modal = $(this);
+		var form = modal.find('form');
+		var baseurl = form.data('baseurl');
+		
+		if(!baseurl.endsWith('/'))
+			baseurl += '/';
+		
+		form.attr('action', baseurl + id);
+		modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <b>' + id + '</b> (<b>' + descricao + '</b>)?');
+	});
+	
+	// Tooltip
 	$('[rel="tooltip"]').tooltip();
+	
+	// MaskMoney
+	$('.currency').maskMoney({
+		decimal: ',',
+		thousands: '.',
+		allowzero: true
+	});
 });
