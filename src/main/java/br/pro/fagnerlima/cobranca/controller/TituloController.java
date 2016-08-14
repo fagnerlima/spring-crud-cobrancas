c4e4b8580baa97181c2224a9cb6f6322c17ce2b9
Fagner Lima <contato@fagnerlima.pro.br>
2016-08-13 22:44:22 -0300

    Componente BootstrapDatePicker

--- src/main/java/br/pro/fagnerlima/cobranca/controller/TituloController.java
+++ src/main/java/br/pro/fagnerlima/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -50,10 +51,15 @@ public class TituloController {
 		if(errors.hasErrors())
 			return "CadastroTitulo";
 		
-		titulos.save(titulo);
-		redirectAttributes.addFlashAttribute("mensagem", "Título salvo com sucesso.");
-		
-		return "redirect:/titulos/novo";
+		try {
+			titulos.save(titulo);
+			redirectAttributes.addFlashAttribute("mensagem", "Título salvo com sucesso.");
+			
+			return "redirect:/titulos/novo";
+		} catch (DataIntegrityViolationException e) {
+			errors.rejectValue("dataVencimento", null, "Formato de data inválido.");
+			return CADASTRO_VIEW;
+		}
 	}
 	
 	@RequestMapping("/{id}")
