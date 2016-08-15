cde7fd96036cb1bd729375be067102445a08c4f8
Fagner Lima <contato@fagnerlima.pro.br>
2016-08-14 21:36:00 -0300

    Recebendo a cobrança com Ajax e ajustando o layout da tabela

--- src/main/java/br/pro/fagnerlima/cobranca/service/CadastroTituloService.java
+++ src/main/java/br/pro/fagnerlima/cobranca/service/CadastroTituloService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import br.pro.fagnerlima.cobranca.model.StatusTitulo;
 import br.pro.fagnerlima.cobranca.model.Titulo;
 import br.pro.fagnerlima.cobranca.repository.Titulos;
 
@@ -23,6 +24,14 @@ public class CadastroTituloService {
 	
 	public void excluir(Long id) {
 		titulos.delete(id);
+	}
+	
+	public String receber(Long id) {
+		Titulo titulo = titulos.findOne(id);
+		titulo.setStatus(StatusTitulo.RECEBIDO);
+		titulos.save(titulo);
+		
+		return StatusTitulo.RECEBIDO.getDescricao();
 	}
 	
 }
--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -25,4 +25,29 @@ $(function() {
 		thousands: '.',
 		allowzero: true
 	});
+	
+	// Atualizar status
+	$('.atualizar-status').click(function(event) {
+		event.preventDefault();
+		
+		var botaoReceber = $(event.currentTarget);
+		var urlReceber = botaoReceber.attr('href');
+
+		var response = $.ajax({
+			url: urlReceber,
+			type: 'PUT'
+		});
+		
+		response.done(function(e) {
+			var id = botaoReceber.data('id');
+			$('[data-role=' + id + ']').html('<span class="label label-success">' + e + '</span>');
+			
+			botaoReceber.hide();
+			
+		});
+		
+		response.fail(function(e) {
+			alert('Erro no recebimento da cobrança.');
+		});
+	});
 });
